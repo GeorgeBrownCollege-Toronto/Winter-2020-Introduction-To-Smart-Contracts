12202e03e7d46d6db4e409fd984d3927f67e3fb6
dhruvinparikh <imsilent93@gmail.com>
2021-02-28 18:06:03 -0500

    beautify

--- dapp/js/app.js
+++ dapp/js/app.js
@@ -1,138 +1,138 @@
 var url = "ws://127.0.0.1:7545";
 
 var abi = [
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_n",
-				"type": "uint256"
-			}
-		],
-		"name": "decrement",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_n",
-				"type": "uint256"
-			}
-		],
-		"name": "increment",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "caller",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "Decrement",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "caller",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "Increment",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_x",
-				"type": "uint256"
-			}
-		],
-		"name": "set",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "caller",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "Set",
-		"type": "event"
-	},
-	{
-		"inputs": [],
-		"name": "get",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "getOwner",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_n",
+        type: "uint256",
+      },
+    ],
+    name: "decrement",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_n",
+        type: "uint256",
+      },
+    ],
+    name: "increment",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "caller",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Decrement",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "caller",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Increment",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_x",
+        type: "uint256",
+      },
+    ],
+    name: "set",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "caller",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Set",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "get",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getOwner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
 ];
 
 var caller;
 var contractInstance;
-var contractAddress = "0x454Fc4a559bd801E569d6586445d2880A1B07ad2";
+var contractAddress = "0x3e63edC253955742E914f716b05e3aeF4d7F22ED";
 
 $("#setForm").submit((e) => {
   e.preventDefault();
@@ -143,9 +143,11 @@ $("#setForm").submit((e) => {
     .on("receipt", () => {
       // Nothing to do here. If the transaction is successful, it will trigger
       // events that we're already monitoring.
-    }).on('error', function(_err, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
-        error(_err)
-    });;
+    })
+    .on("error", function (_err, receipt) {
+      // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+      error(_err);
+    });
 });
 
 function log(message) {
@@ -154,39 +156,50 @@ function log(message) {
 }
 
 function error(message) {
-    $('#log').append($('<p>').addClass('dark-red').text(message));
-    $('#log').scrollTop($('#log').prop('scrollHeight'));
+  $("#log").append($("<p>").addClass("dark-red").text(message));
+  $("#log").scrollTop($("#log").prop("scrollHeight"));
 }
 
 $("#decrementForm").submit((e) => {
-    e.preventDefault();
-    log("Decrementing storedData...");
-    contractInstance.methods
-      .decrement($("#decrementNumber").val())
-      .send({ from: caller })
-      .on("receipt", () => {
-        // Nothing to do here. If the transaction is successful, it will trigger
-        // events that we're already monitoring.
-      }).on('error', function(_err, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
-          error(_err)
-      });;
-  });
+  e.preventDefault();
+  log("Decrementing storedData...");
+  contractInstance.methods
+    .decrement($("#decrementNumber").val())
+    .send({ from: caller })
+    .on("receipt", () => {
+      // Nothing to do here. If the transaction is successful, it will trigger
+      // events that we're already monitoring.
+    })
+    .on("error", function (_err, receipt) {
+      // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+      error(_err);
+    });
+});
 
-  $("#incrementForm").submit((e) => {
-    e.preventDefault();
-    log("Incrementing storedData...");
-    contractInstance.methods
-      .increment($("#incrementNumber").val())
-      .send({ from: caller })
-      .on("receipt", () => {
-        // Nothing to do here. If the transaction is successful, it will trigger
-        // events that we're already monitoring.
-      }).on('error', function(_err, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
-          error(_err)
-      });;
-  });
+$("#incrementForm").submit((e) => {
+  e.preventDefault();
+  log("Incrementing storedData...");
+  contractInstance.methods
+    .increment($("#incrementNumber").val())
+    .send({ from: caller })
+    .on("receipt", () => {
+      // Nothing to do here. If the transaction is successful, it will trigger
+      // events that we're already monitoring.
+    })
+    .on("error", function (_err, receipt) {
+      // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+      error(_err);
+    });
+});
 
+var start = +new Date();
 function updateUI(storedData, owner) {
+  // Delay first UI update until the spinner has been visible for 1 second.
+  if (+new Date() - start < 1000) {
+    return window.setTimeout(() => {
+      updateUI(storedData, owner);
+    }, 1000 - +new Date() + start);
+  }
   $("#storedData").text(storedData.toLocaleString());
   $("#owner").text(owner.toLocaleString());
 }
@@ -198,27 +211,23 @@ function loadData(blockNumber) {
 
   // Callback that waits for all three pieces of data before proceeding
   function updateWhenDone() {
-    if (storedData !== undefined || owner !== undefined) {
+    if (storedData !== undefined && owner !== undefined) {
       log("Updating UI.");
       updateUI(storedData, owner);
     }
   }
 
-  contractInstance.methods
-    .get()
-    .call(blockNumber, (err, _storedData) => {
-      if (err) return error(err);
-      storedData = _storedData;
-      updateWhenDone();
-    });
+  contractInstance.methods.get().call(blockNumber, (err, _storedData) => {
+    if (err) return error(err);
+    storedData = _storedData;
+    updateWhenDone();
+  });
 
-    contractInstance.methods
-    .getOwner()
-    .call(blockNumber, (err, _owner) => {
-      if (err) return error(err);
-      owner = _owner;
-      updateWhenDone();
-    });
+  contractInstance.methods.getOwner().call(blockNumber, (err, _owner) => {
+    if (err) return error(err);
+    owner = _owner;
+    updateWhenDone();
+  });
 }
 
 async function initialize() {
@@ -228,7 +237,7 @@ async function initialize() {
 
     // Handler for events that necessitate a UI update
     function changeHandler(err, data) {
-      if (err) return error(err);
+	    if (err) return error(err);
 
       // Only if we haven't already fetched data for this block
       if (data.blockNumber > lastSeenBlock) {
